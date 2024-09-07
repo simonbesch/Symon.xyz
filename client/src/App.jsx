e641c610531f53723cf3e1a4fec20138945db222
simonbesch <simonbescheron@gmail.com>
2024-09-07 16:13:02 +0200

    easter egg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,20 @@ import clip from "./assets/clip2.mp4";
 import Poster from "./assets/Poster.jpg";
 
 function App() {
+  console.info(
+    `
+ ░▒▓███████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓██████████████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░       ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ 
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░ 
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░    ░▒▓██▓▒░  
+ ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░       ░▒▓██████▓▒░ ░▒▓██████▓▒░   ░▒▓██▓▒░    
+       ░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░    ░▒▓██▓▒░      
+       ░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓██▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░        
+░▒▓███████▓▒░   ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓██▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓████████▓▒░ 
+                                                                                                                        
+                                                                                                                                                           
+  `
+  );
+
   const isTabletOrMobile = useMediaQuery({ query: "(max-width: 699px)" });
   const isDesktopOrLaptop = useMediaQuery({ query: "(min-width: 700px)" });
   return (
@@ -18,9 +32,16 @@ function App() {
       </main>
       <Footer />
 
-      <video autoPlay loop muted poster={Poster} className="background-video">
-        <source src={clip} type="video/mp4" />
-        <source src={clip} type="video/ogg" />
+      <video
+        autoPlay
+        loop
+        muted
+        poster={Poster}
+        className="background-video"
+        alt="poster"
+      >
+        <source src={clip} type="video/mp4" alt="poster" />
+        <source src={clip} type="video/ogg" alt="poster" />
       </video>
     </div>
   );
--- client/src/error-page.jsx
+++ client/src/error-page.jsx
@@ -5,7 +5,11 @@ function ErrorPage() {
 
   return (
     <div id="error-page">
-      <h1>Tu m'a l'air perdu ?</h1>
+      <h1>Tu m'as l'air perdu ?</h1>
+      <img
+        src="https://veraicona.hypotheses.org/files/2017/11/confused-travolta-original-pulp-fiction-animated-gif.gif"
+        alt="Lost"
+      />
     </div>
   );
 }
